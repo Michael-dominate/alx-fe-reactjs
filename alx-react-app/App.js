45af90b2d89ff456da4be02cb1fabfc6778d1e8f
Michael Mwenda <michaelkithinji680@gmail.com>
2025-07-13 20:37:50 +0300

    feat: add WelcomeMessage component and integrate into App

---
+++ alx-react-app/App.js
@@ -0,0 +1,10 @@
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomeMessage />
+      {/* Other existing content */}
+    </div>
+  );
+}
