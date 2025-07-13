1e4fff75bd8bfcefe3cdd901662f32968081c4fd
Michael Mwenda <michaelkithinji680@gmail.com>
2025-07-13 14:51:10 +0300

    added App.jsx

---
+++ alx-react-app/App.jsx
@@ -0,0 +1,12 @@
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <>
+      <WelcomeMessage />
+      {/* ... other existing code ... */}
+    </>
+  );
+}
+
+export default App;
